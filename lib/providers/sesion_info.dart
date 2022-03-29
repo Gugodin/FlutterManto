b44ee1c73eabb9d035829a3e9ec5958734294b95
Javier Gutierrez <gugodin@hotmail.com>
2022-03-29 14:20:13 -0600

    añandiendo rol en el backend

--- lib/providers/sesion_info.dart
+++ lib/providers/sesion_info.dart
@@ -4,15 +4,18 @@ class SesionInfo with ChangeNotifier {
   int id = 0;
 
   String token = '';
+  String rol = '';
 
   void saveData({required List<dynamic> datos}) {
-    token = datos[1].toString();
     id = datos[0];
+    token = datos[1].toString();
+    rol = datos[2];
     notifyListeners();
 
     Future.delayed(const Duration(milliseconds: 6000000), () {
       id = 0;
       token = '';
+      rol =  '';
       notifyListeners();
     });
   }
